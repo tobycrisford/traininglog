7861f450bee7a1c0424af8b40afd1da5848d048a
Toby Crisford <tobycrisford@gmail.com>
2021-11-02 20:44:56 +0000

    Allowing selection of multiple activities

--- training_log.py
+++ training_log.py
@@ -46,14 +46,18 @@ df['week_start'] = (df['datetime'] - df['weekday'].apply(lambda x: timedelta(day
 #Some Garmin activities are wrongly duplicated - deal with that here
 df = df[~df.duplicated(subset='beginTimestamp',keep='first')]
 
+activities = sorted(list(set(df['activityType'])))
+
 app = Flask(__name__)
 print(__name__)
 
 @app.route('/traininglog')
 def create_training_log():
-    if not (request.args.get('activity') is None):
-        df_out = df[(df['activityType'] == request.args.get('activity')) & \
-                    (df['week_start'] >= datetime.datetime.strptime(request.args.get('start'), '%Y-%m-%d').date()) & \
+    if not (request.args.get('start') is None):
+        rows_to_keep = np.array([False for i in range(len(df))])
+        for activity in activities:
+            rows_to_keep = rows_to_keep | (df['activityType'] == request.args.get(activity))
+        df_out = df[rows_to_keep & (df['week_start'] >= datetime.datetime.strptime(request.args.get('start'), '%Y-%m-%d').date()) & \
                     (df['week_start'] <= datetime.datetime.strptime(request.args.get('end'), '%Y-%m-%d').date())].copy()
     else:
         df_out = df[df['activityType'] == '']
@@ -73,7 +77,7 @@ def create_training_log():
     df_out['total_week_number'] = df_out['total_week_number'] - df_out['total_week_number'].min()
     df_out['total_week_number'] = df_out['total_week_number'].max() - df_out['total_week_number'] #Show log going backwards from present
     
-    option_values = {'activity': sorted(list(set(df['activityType']))), 'start': sorted(list(set(df['week_start'].astype(str)))),
+    option_values = {'start': sorted(list(set(df['week_start'].astype(str)))),
                      'end': sorted(list(set(df['week_start'].astype(str)))), 'measure': {'distance','duration','elevation'}}
     
     max_val = df_out['output'].max()
@@ -88,6 +92,7 @@ def create_training_log():
     
     return render_template('training_log.html', output = df_out,
                            option_values = option_values, request_args = request.args,
-                           elapsed_weeks = elapsed_weeks, week_totals = week_totals)
+                           elapsed_weeks = elapsed_weeks, week_totals = week_totals,
+                           activities = activities)
 
 app.run(debug=True)
